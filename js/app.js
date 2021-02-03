46b9104839308a5fcb01f1761ba4f5bfee06ac95
Aibek <aibekdv@gmail.com>
2021-02-04 03:11:00 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,64 @@
+$( document ).ready(function() {
+    $(".catalog-btn").click(function(){
+        $(".catalog-block").toggleClass("height");
+      });
+
+      // $(".slider-left").slick({
+      //   dots: true,
+      //   infinite: false,
+      //   arrows:false,
+      //   speed: 300,
+      //   autoplay:true
+      // })
+      // $(".slider-right").slick({
+      //   infinite: false,
+      //   arrows:false,
+      //   speed: 300,
+      //   vertical:true,
+      //   verticalSwiping: true,
+      //   slidesToShow: 2,
+      //   autoplay: true,
+      // })
+
+  });
+  var swiper = new Swiper('.swiper1', {
+    slidesPerView: 'auto',
+    loop:true,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+    },
+  });
+  var swiper1 = new Swiper(".swiper2",{
+    direction: 'vertical',
+    slidesPerView: 2,
+    spaceBetween:5,
+    freeMode:false,
+    autoplay: {
+      delay: 5000,
+    },
+  })
+  var swiper2 = new Swiper(".swiper3",{
+    slidesPerView: 3,
+    spaceBetween:50,
+    freeMode:false,
+    loop:true,
+    navigation: {
+      nextEl: '.navig .swiper-button-next',
+      prevEl: '.navig .swiper-button-prev',
+    },
+  })
+  var swiper3 = new Swiper(".swiper4",{
+    slidesPerView: 3,
+    spaceBetween:50,
+    freeMode:false,
+    loop:true,
+    navigation: {
+      nextEl: '.navig-client .swiper-button-next',
+      prevEl: '.navig-client .swiper-button-prev',
+    },
+  })
