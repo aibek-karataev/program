f95f5d019ffe4aad3a5d3cc059b7540725ba1a0d
aibek-karataev <aibekdv@gmail.com>
2021-02-12 17:34:10 +0300

    edited

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
   // Modal window
   let modal = document.getElementById("myModal");
   let btn = document.getElementById("modalBtn");
+  let btn1 = document.getElementById("modalBtn1");
   let span = document.getElementsByClassName("close")[0];
 
 btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn1.onclick = function() {
   modal.style.display = "block";
 }
 
@@ -16,52 +20,3 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
-$( document ).ready(function() {
-    $(".catalog-btn").click(function(){
-        $(".catalog-block").toggleClass("height");
-        $(".catalog-btn").toggleClass("he");
-      });
-  });
-  var swiper = new Swiper('.swiper1', {
-    slidesPerView: 'auto',
-    loop:true,
-    spaceBetween: 20,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    autoplay: {
-      delay: 5000,
-    },
-    onlyExternal:false
-  });
-  var swiper1 = new Swiper(".swiper2",{
-    direction: 'vertical',
-    slidesPerView: 2,
-    spaceBetween:5,
-    freeMode:false,
-    autoplay: {
-      delay: 5000,
-    },
-  })
-  var swiper2 = new Swiper(".swiper3",{
-    slidesPerView: 3,
-    spaceBetween:50,
-    freeMode:false,
-    loop:true,
-    navigation: {
-      nextEl: '.navig .swiper-button-next',
-      prevEl: '.navig .swiper-button-prev',
-    },
-  })
-  var swiper3 = new Swiper(".swiper4",{
-    slidesPerView: 3,
-    spaceBetween:50,
-    freeMode:false,
-    loop:true,
-    navigation: {
-      nextEl: '.navig-client .swiper-button-next',
-      prevEl: '.navig-client .swiper-button-prev',
-    },
-  })
-
---
+++ js/slide.js
@@ -0,0 +1,49 @@
+$( document ).ready(function() {
+    $(".catalog-btn").click(function(){
+        $(".catalog-block").toggleClass("height");
+        $(".catalog-btn").toggleClass("he");
+      });
+  });
+  var swiper = new Swiper('.swiper1', {
+    slidesPerView: 'auto',
+    loop:true,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    autoplay: {
+      delay: 5000,
+    },
+    onlyExternal:false
+  });
+  var swiper1 = new Swiper(".swiper2",{
+    direction: 'vertical',
+    slidesPerView: 2,
+    spaceBetween:5,
+    freeMode:false,
+    autoplay: {
+      delay: 5000,
+    },
+  })
+  var swiper2 = new Swiper(".swiper3",{
+    slidesPerView: 3,
+    spaceBetween:50,
+    freeMode:false,
+    loop:true,
+    navigation: {
+      nextEl: '.navig .swiper-button-next',
+      prevEl: '.navig .swiper-button-prev',
+    },
+  })
+  var swiper3 = new Swiper(".swiper4",{
+    slidesPerView: 3,
+    spaceBetween:50,
+    freeMode:false,
+    loop:true,
+    navigation: {
+      nextEl: '.navig-client .swiper-button-next',
+      prevEl: '.navig-client .swiper-button-prev',
+    },
+  })
+
